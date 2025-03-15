796e38afa3dce0dce1b202c3844fbbb1719eb1a1
Hugo <h.ferreira147@gmail.com>
2025-03-15 20:31:14 -0300

    fix: corrige sintaxe do arquivo de rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
+import { RouteObject } from "react-router-dom";
 import Configuracoes from "../features/perfil/pages/Configuracoes";
 
-// Dentro do array de rotas protegidas
-{
-  path: "/configuracoes",
-  element: <Configuracoes />,
-}, 
+export const protectedRoutes: RouteObject[] = [
+  {
+    path: "/configuracoes",
+    element: <Configuracoes />,
+  },
+];
+
+export const publicRoutes: RouteObject[] = [];
